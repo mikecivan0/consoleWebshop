a3c7fd1c81a8de637e80918c15f86382386ecab3
Ivan Mikec <mikecivan0@gmail.com>
2021-10-12 12:33:12 +0200

    hibernate configuration and added Product entity class

---
+++ src/main/java/hr/mikec/webstore/model/Product.java
@@ -0,0 +1,78 @@
+package hr.mikec.webstore.model;
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    public Product() {
+    }
+
+    public Product(String name, int sku, int quantity, BigDecimal price) {
+        this.name = name;
+        this.sku = sku;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column()
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int sku;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSku() {
+        return sku;
+    }
+
+    public void setSku(int sku) {
+        this.sku = sku;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    toString
+
+}
