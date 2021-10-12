6a2d0b125f023fa547e1221b2675fa2894bf67b7
Ivan Mikec <mikecivan0@gmail.com>
2021-10-12 17:48:18 +0200

    added tools class

---
+++ src/main/java/hr/mikec/webstore/util/Tools.java
@@ -0,0 +1,91 @@
+package hr.mikec.webstore.util;
+
+import java.util.Scanner;
+
+public class Tools {
+
+    public static Scanner scanner;
+
+    public static int parseInt(String message, String errorMessage, int min) {
+        int number = 0;
+        while (true) {
+            System.out.print(message);
+            try {
+                number = Integer.parseInt(scanner.nextLine());
+                if (number < min) {
+                    System.out.println("Enter number greater than " + min);
+                    continue;
+                }
+                break;
+            } catch (Exception e) {
+                System.out.println(errorMessage);
+            }
+        }
+        return number;
+    }
+
+    public static double parseDouble(String message, String errorMessage, double min) {
+        double number = 0;
+        while (true) {
+            System.out.print(message);
+            try {
+                number = Double.parseDouble(scanner.nextLine());
+                if (number < min) {
+                    System.out.println("Enter number greater than " + min);
+                    continue;
+                }
+                break;
+            } catch (Exception e) {
+                System.out.println(errorMessage);
+            }
+        }
+        return number;
+    }
+
+    public static String parseString(String message, String errorMessage, int minLength) {
+        String str = "";
+        while (true) {
+            System.out.print(message);
+            str = scanner.nextLine();
+            if (str.trim().equals("")) {
+                System.out.println(errorMessage);
+            }
+            if (str.length() < minLength) {
+                System.out.println("Enter at least " + minLength + " characters");
+                continue;
+            }
+            break;
+        }
+        return str;
+    }
+
+    public static boolean parseYesNo(String message, String errorMessage) {
+        String str;
+        while (true) {
+            System.out.print(message);
+            str = scanner.nextLine().trim().toLowerCase();
+            if (str.equals("yes")) {
+                return true;
+            }
+            if (str.equals("no")) {
+                return false;
+            }
+            System.out.println(errorMessage);
+        }
+    }
+
+    public static void printHeading(String title) {
+        title = "-" + title + "-";
+        String dashes = "";
+        for (int i = 1; i <= title.length(); i++) {
+            dashes += "-";
+        }
+        System.out.println();
+        System.out.println(dashes);
+        System.out.println(title);
+        System.out.println(dashes);
+
+    }
+
+}
+
